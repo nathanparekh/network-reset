2bcb882d7bb153bca45ce7693d4abd63e1c9d70e
cklaassen <klaassenconnor@gmail.com>
2020-03-06 10:48:07 -0800

    Added support for native macOS python2

--- network.py
+++ network.py
@@ -3,22 +3,23 @@ from __future__ import print_function
 import platform
 import subprocess
 import time
+import os
 from datetime import datetime as dt
 
 
 def timestamp():
     now = dt.now()
-    print("[" + str(now.hour).zfill(2) + ":" + str(now.minute).zfill(2) + ":" + str(now.second).zfill(2) + "]", end=" ",
-          flush=True)
+    print("[" + str(now.hour).zfill(2) + ":" + str(now.minute).zfill(2) + ":" + str(now.second).zfill(2) + "]", end=" ")
 
 
 def check_internet(host="google.com"):
     timestamp()
-    print("Testing " + host + "... ", end="", flush=True)
+    print("Testing " + host + "... ", end="")
+    FNULL = open(os.devnull, 'w')
     if platform.system() == "Darwin":
-        ping_result = subprocess.call(["ping", "-c", "1", host], shell=False, stdout=subprocess.DEVNULL)
+        ping_result = subprocess.call(["ping", "-c", "1", host], shell=False, stdout=FNULL)
     elif platform.system() == "Windows":
-        ping_result = subprocess.call(["ping", "-n", "1", host], shell=False, stdout=subprocess.DEVNULL)
+        ping_result = subprocess.call(["ping", "-n", "1", host], shell=False, stdout=FNULL)
     else:
         raise OSError("Platform not supported.")
     if ping_result == 0:
@@ -33,12 +34,13 @@ print("Starting... use CTRL-C to exit.")
 while True:
     if not check_internet():
         timestamp()
-        print("Resetting... ", end="", flush=True)
+        print("Resetting... ", end="")
+        FNULL = open(os.devnull, 'w')
         if platform.system() == "Darwin":
             subprocess.call(["networksetup", "-setairportpower", "Wi - Fi", "off"],
-                            shell=False, stdout=subprocess.DEVNULL)
+                            shell=False, stdout=FNULL)
             subprocess.call(["networksetup", "-setairportpower", "Wi - Fi", "on"],
-                            shell=False, stdout=subprocess.DEVNULL)
+                            shell=False, stdout=FNULL)
             # TODO: fix
         elif platform.system() == "Windows":
             subprocess.call(["netsh", "wlan", "disconnect"])
